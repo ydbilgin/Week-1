ce170e43967051cb1639069025f8dd5296ee1ed4
ydbilgin <yasinderyabilgin@gmail.com>
2023-09-13 22:33:31 +0300

    Siralama

---
+++ Siralama/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a,b,c;
+
+        System.out.println("Birinci sayıyı giriniz : ");
+        a= input.nextInt();
+        System.out.println("İkinci sayıyı giriniz : ");
+        b= input.nextInt();
+        System.out.println("Üçüncü sayıyı giriniz : ");
+        c= input.nextInt();
+
+        if ((a<b) && (a<c)){
+            if (b<c){
+                System.out.println(a + " < " + b + " < " + c);
+            }else{
+                System.out.println(a + " < " + c + " < " + b);
+            }
+        } else if ((b<a) && (b<c)) {
+            if (a<c){
+                System.out.println(b + " < " + a + " < " + c);
+            }else {
+                System.out.println(b + " < " + c + " < " + a);
+            }
+
+        }else if ((c<a) && (c<b)){
+            if (b<a){
+                System.out.println(c + " < " + b + " < " + a);
+            }else {
+                System.out.println(c + " < " + a + " < " + b);
+            }
+        }
+
+
+    }
+}
