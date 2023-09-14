8779686fb0a2626b023a3d02e9bbd36ba78857bd
ydbilgin <yasinderyabilgin@gmail.com>
2023-09-14 19:53:22 +0300

    Kombinasyon

--- Kombinasyon/src/Main.java
+++ Kombinasyon/src/Main.java
@@ -14,16 +14,19 @@ public class Main {
             System.exit(0);
         }
         fark = n-r;
-
+        // Formülde bulunan n! formülü
         for (int i = 1 ; i <= n ; i++ ){
             nFac = i * nFac;
         }
+        // Formülde bulunan r! formülü
         for (int i = 1 ; i <= r ; i++ ){
             rFac = i * rFac;
         }
+        // Formülde bulunan (n-r)! formülü
         for (int i = 1 ; i <= fark ; i++ ){
             farkToplam = i * farkToplam;
         }
+        // Formül
         kombinasyon = nFac/(rFac*farkToplam);
         System.out.println("C(n,r) = " + kombinasyon );
 
