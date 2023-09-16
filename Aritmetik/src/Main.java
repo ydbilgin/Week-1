c7f750ad343b62c9c4d279a40ec2a677212eea86
ydbilgin <yasinderyabilgin@gmail.com>
2023-09-17 01:19:39 +0300

    Aritmetik

--- Aritmetik/src/Main.java
+++ Aritmetik/src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
