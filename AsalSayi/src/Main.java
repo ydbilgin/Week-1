61530db17e1f0d795757ea4c6990930a406d66ab
ydbilgin <yasinderyabilgin@gmail.com>
2023-09-16 06:04:54 +0300

    AsalSayilar

---
+++ AsalSayi/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal=false;
+                }
+            }
+            if (asal) {
+                numberList.add(i);
+            }
+        }
+        System.out.println(numberList);
+    }
+}
--- deneme/src/Main.java
+++ deneme/src/Main.java
@@ -1,44 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+package Week1;
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input=new Scanner(System.in);
+        int n;
+        System.out.print("Basamak sayısını giriniz: ");  //basamak sayısını kullanıcıdan alıyoruz.
+        n=input.nextInt();
 
-        int sayi;
-        List<Integer> degerler = new ArrayList<>();
-        System.out.println("Kaç tane sayı gireceksiniz: ");
-        sayi = input.nextInt();
+        for(int i=1;i<=n;i++){
 
-
-        int i = 1;
-        while (i <= sayi) {
-            System.out.println(i + ". sayıyı giriniz:");
-            int a = input.nextInt();
-            degerler.add(a);
-            i++;
-
-        }
-        int enKucuk = degerler.get(0);
-        for (int k = 0; k < degerler.size(); k++) {
-            if (degerler.get(k) < enKucuk) {
-                enKucuk = degerler.get(k);
+            for(int b=1;b<=(i-1);b++){   //her basamak için boşluk sayımız i den 1 eksik oluyor.
+                System.out.print(" ");
             }
-        }
-
-        System.out.println("En küçük değer: " + enKucuk);
-
-
-        int enBuyuk = degerler.get(0);
-        for (int j = 0; j < degerler.size(); j++) {
-            if (degerler.get(j) > enBuyuk) {
-                enBuyuk = degerler.get(j);
+            for (int y=1;y<=(((n-i)*2)+1);y++){  // her basamak için yıldız degerimiz ((n-i)*2)+1 kadar oluyor.
+                System.out.print("*");
             }
+            System.out.println();
         }
-
-        System.out.println("En büyük değer: " + enBuyuk);
 
     }
 }
