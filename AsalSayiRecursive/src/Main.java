7d58d9ddb8b05af708da4fccbf9cec49235ef1bc
ydbilgin <yasinderyabilgin@gmail.com>
2023-09-19 07:52:05 +0300

    AsalSayiRecursive

--- AsalSayiRecursive/src/Main.java
+++ AsalSayiRecursive/src/Main.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    static boolean asal(int a) {
-
-        for (int i = 2 ; i < a;i++){
-            if (a % i == 0){
-                boolean sonuc = false;
+    static boolean isAsal(int a, int b) {
+        while (b > 2) {
+            //a sayısından 1 eksiğiyle başlayarak 2 ye kadar modunu aldırıyorum. eğer bölünebiliyorsa asal sayı değil bölünemiyorsa asal sayı
+            if (a % b == 0) {
+                System.out.println(a + " asal sayı değildir.");
+                return false;
+            } else if (b == 3) {
+                System.out.println(a + " asal sayıdır .");
+                return true;
             }
+            b--;
         }
-
+        return isAsal(a, b - 1);
 
     }
 
     public static void main(String[] args) {
-        int a;
+        int a, b;
         Scanner input = new Scanner(System.in);
-        System.out.println("Taban değerini giriniz : ");
+        System.out.println("Sayı : ");
         a = input.nextInt();
+        b = a - 1;
 
+        isAsal(a, b);
 
 
     }
 }
+
+
